d55e8b7dc7d3b566c859856301dc100618818b09
Vojtech Salbaba <darktatka@gmail.com>
2009-11-04 23:44:00 +0100

    Changes trying to highlight a possible bug in formtastic i8n support

---
+++ config/locales/cz.rb
@@ -0,0 +1,15 @@
+{:cz => {
+  :formtastic => {
+    :labels => {
+      :post => {
+        :title => "Title (i18n)",
+        :body => "Write something... (i18n)"
+        },
+    :actions => {
+        :create => "Create my {{model}} (i18n)",
+        :update => "Save changes (i18n)"}
+      }
+    }
+  }
+}
+
