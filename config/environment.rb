6378c6bdcf1c9d36dc7dc73de5bf3a54ca2edc71
Vojtech Salbaba <darktatka@gmail.com>
2009-11-18 09:47:10 +0100

    removed formtastic as a plugin and added it as a gem.

--- config/environment.rb
+++ config/environment.rb
@@ -17,7 +17,7 @@ Rails::Initializer.run do |config|
   # Specify gems that this application depends on and have them installed with rake gems:install
   #config.gem 'validatious-on-rails'
   config.gem 'validation_reflection'
-
+  config.gem 'formtastic'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
